82000f133869f0de26900b2e23cd352ae8ff0bcd
Ivan Pantic <ivanpantic82@gmail.com>
2017-01-26 15:05:15 +0100

    Naming and stuff

--- app.js
+++ app.js
@@ -1,6 +1,4 @@
 var yauzl = require("yauzl");
-var fs = require("fs");
-var path = require("path");
 
 yauzl.open(__dirname + "/test.zip", {lazyEntries: true}, function (err, zipfile) {
 	if (err) throw err;
